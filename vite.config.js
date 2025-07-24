986acfe84b67249b1b5299140bc1bdf9fcdc722b
Sadia Shakil <89252934+iamsadiashakil@users.noreply.github.com>
2025-07-24 20:03:12 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-   base: '/vexa_website/',
+  base: '/vexa_website/', 
   plugins: [react()],
-  // No need to specify postcss config path if it's in the project root
 })
