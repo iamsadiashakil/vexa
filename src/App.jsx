a05d004fa357639eaa782ad99c8cb801b00ec935
Sadia Shakil <89252934+iamsadiashakil@users.noreply.github.com>
2025-07-26 13:36:37 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
     <Router>
       <LayoutManager>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/vexa_website" element={<Home />} />
           <Route path="/about" element={<AboutUs />} />
           <Route path="/services" element={<Services />} />
           <Route path="/portfolio" element={<Portfolio />} />
